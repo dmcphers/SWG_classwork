60f0f4d39a52a8b389370fa4fc412219d0f4501a
Drew McPherson <drewmac1971@gmail.com>
2020-10-11 16:56:14 -0500

    added hello world program

---
+++ M1/HelloWorldSample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("My name is Drew!");
+            Console.WriteLine("Nice to be here!");
+            Console.ReadLine();
+        }
+    }
+}
