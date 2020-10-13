d6be0b706788b149f16459cfa858db87ea39abba
Drew McPherson <drewmac1971@gmail.com>
2020-10-12 22:12:51 -0500

    added in_a_bucket_exercise

---
+++ M1/InABucket/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace InABucket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // You can declare all KINDS of variables.
+            String walrus;
+            double piesEaten;
+            float weightOfTeacupPig;
+            int grainsOfSand;
+
+            // But declaring them just sets up the space for data.
+            // If you want to use the variable, you have to put data IN it first!
+            walrus = "Sir Leroy Jenkins III";
+            piesEaten = 42.1;
+            weightOfTeacupPig = 25.6F;
+            grainsOfSand = 1000000;
+
+            Console.WriteLine("Meet my pet walrus, " + walrus);
+            Console.Write("He was a bit hungry today and ate this many pies: ");
+            Console.WriteLine(piesEaten);
+            Console.WriteLine("The weight of my pet pig is " + weightOfTeacupPig + " pounds.");
+            Console.WriteLine("That is about the equivalent weight of " + grainsOfSand + " grains of sand.");
+        }
+    }
+}
