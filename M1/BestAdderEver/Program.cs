0aced45dae0d2f7c3ba13afd107b2774f8387257
Drew McPherson <drewmac1971@gmail.com>
2020-10-12 22:30:48 -0500

    added best_adder_ever exercise

---
+++ M1/BestAdderEver/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BestAdderEver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 6;
+            int num3 = 7;
+            int num4 = num1 + num2 + num3;
+            Console.WriteLine("The sum of " + num1 + " plus " + num2 + " plus " + num3 + " equals " + num4);
+        }
+    }
+}
