9d5cc925d12464055594809d6d7f14967738d7c3
Drew McPherson <drewmac1971@gmail.com>
2020-10-14 17:51:17 -0500

    added guess me more exercise

---
+++ M1/GuessMeMore/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace GuessMeMore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int secNumber = -27;
+            string userGuess;
+            bool isValid = false;
+
+
+            Console.WriteLine("I've chosen a signed integer number between -100 and 100. Bet you can't guess it!");
+            do
+            {
+                userGuess = Console.ReadLine();
+
+                isValid = Int32.TryParse(userGuess, out int guess);
+                if (isValid == true)
+                {
+                    if (guess == secNumber)
+                    {
+                        Console.WriteLine("That was it, congratulatons!");
+                    }
+                    else if (guess < secNumber)
+                    {
+                        Console.WriteLine("Your guess is too low. Try again!");
+                        isValid = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your guess is too high. Try again!");
+                        isValid = false;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("That was not a signed integer value between -100 and 100, please try again.");
+                }
+            } while (isValid == false);
+
+            Console.ReadLine();
+        }
+    }
+}
