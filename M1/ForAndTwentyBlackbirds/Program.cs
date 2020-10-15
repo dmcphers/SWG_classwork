f341b83a825e0b05d11df4f00926a8e72a1ce51a
Drew McPherson <drewmac1971@gmail.com>
2020-10-14 21:34:43 -0500

    added for and twenty blackbirds exercise

---
+++ M1/ForAndTwentyBlackbirds/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ForAndTwentyBlackbirds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int birdsInPie = 0;
+            for (int i = 0; i < 24; i++)    // updated the stop condition from 20 to 24 so will now print out 24 lines and total
+            {
+                Console.WriteLine("Blackbird #" + i + " goes into the pie!");
+                birdsInPie++;
+            }
+
+            Console.WriteLine("There are " + birdsInPie + " birds in there!");
+            Console.WriteLine("Quite the pie full!");
+            Console.ReadLine();
+        }
+    }
+}
