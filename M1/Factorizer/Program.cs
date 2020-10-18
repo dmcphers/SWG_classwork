83151102780b105e89088b374876aa300e020826
Drew McPherson <drewmac1971@gmail.com>
2020-10-17 21:24:55 -0500

    added factorizer and fizzbuzz exercises

---
+++ M1/Factorizer/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Factorizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What number would you like to factor? ");
+            string strNum = Console.ReadLine();
+            Console.WriteLine("The factors of " + strNum + " are:");
+
+            bool intNum = false;
+            intNum = Int32.TryParse(strNum, out int userNum);
+            int counter = 0;
+
+            do
+            {
+                if (intNum == true)
+                {
+                    for (int i = 1; i <= userNum; i++)
+                    {
+                        if (userNum % i == 0)
+                        {
+                            Console.Write(i + " ");
+                            counter++;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("That is not a valid number, please try again");
+                }
+
+            } while (intNum == false);
+
+            Console.WriteLine();
+            Console.WriteLine("The number " + userNum + " has " + counter + " factors");
+
+            int[] factors = new int[counter];
+            int factorIndex = 0;
+            for (int i = 1; i <= userNum; i++)
+            {
+                if (userNum % i == 0)
+                {
+                    factors.SetValue(i, factorIndex);
+                    factorIndex++;
+                }
+            }
+
+            int sum = 0;
+            for (int i = 0; i < factors.Length - 1; i++)
+            {
+                sum += factors[i];
+                if (sum == userNum)
+                {
+                    Console.WriteLine(userNum + " is a perfect number");
+                }
+            }
+
+            if ((factors[0] == 1) && (factors[1] == userNum))
+            {
+                Console.WriteLine(userNum + " is a prime number");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ M1/FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Execute();
+            Console.ReadLine();
+        }
+
+        static void Execute()
+        {
+            //TODO:  Implement FizzBuzz
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine(i + " FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine(i + " Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine(i + " Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
