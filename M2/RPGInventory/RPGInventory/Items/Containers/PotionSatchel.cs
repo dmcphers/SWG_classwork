e2a2db6a7329906b2c5a5d7a3c5dc35dbcd91219
Drew McPherson <drewmac1971@gmail.com>
2020-11-17 22:39:14 -0600

    added RPG Inventory program

---
+++ M2/RPGInventory/RPGInventory/Items/Containers/PotionSatchel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGInventory.Items.Containers
+{
+    public class PotionSatchel : SpecificContainer
+    {
+        public PotionSatchel() : base(4, ItemType.Potion)
+        {
+
+        }
+    }
+}
---
+++ M2/RPGInventory/RPGInventory/Items/Containers/WetPaperSack.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGInventory.Items.Containers
+{
+    public class WetPaperSack : WeightRestrictedContainer
+    {
+        public WetPaperSack() : base(8, 3)
+        {
+            Name = "Wet paper sack";
+            Description = "Damp and flimsy";
+            Type = ItemType.Container;
+            Weight = 1;
+        }
+    }
+}
